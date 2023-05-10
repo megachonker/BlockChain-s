4d45cfe965d2d80af86b3bfae1c61a63387e926a
cottelle <Cottelle@users.noreply.github.com>
2023-05-10 22:53:44 +0200

    Add the number of the node and the finder

--- src/block_chain/mod.rs
+++ src/block_chain/mod.rs
@@ -6,10 +6,12 @@ use std::hash::Hasher;
 const HASH_MAX: u64 = 1000000000000;
 #[derive(Debug)]
 pub struct Block {
+    number : u64,
     id: u64,
     last_block: u64,
     transactions: Vec<Transaction>,
     answer: u64,
+    finder : u64,       //Who find the answer
 }
 #[derive(Debug)]
 pub struct Transaction {
@@ -25,38 +27,42 @@ fn hash_int(value: u64) -> u64 {
 }
 
 impl Block {
-    pub fn new(last_block: u64, answer: u64, transactions: Vec<Transaction>) -> Block {
+    pub fn new(last_block: u64, answer: u64, transactions: Vec<Transaction>, number : u64, finder: u64) -> Block {
         let id = answer; //for the momment
         return Block {
-            id: id,
+            number : number,
+            id : id,
             last_block: last_block,
             transactions: transactions,
             answer: answer,
+            finder : finder,
         };
     }
 
     pub fn verification(&self) -> bool {
         hash_int(self.last_block.wrapping_add(self.answer)) < HASH_MAX
     }
-    pub fn new_block(&self, new_transa: Vec<Transaction>, answer: u64) -> Block {
+    pub fn new_block(&self, new_transa: Vec<Transaction>, answer: u64, finder : u64) -> Block {
         Block {
+            number : self.number+1,
             id: answer,
             last_block: self.id,
             transactions: new_transa,
             answer: answer,
+            finder : finder,
         }
     }
 }
 
 pub fn mine(last_block: &Block) -> u64 {
-    let mut hasher: DefaultHasher = DefaultHasher::new();
+    // let mut hasher: DefaultHasher = DefaultHasher::new();
     let last_id = last_block.id;
     let mut number;
     let mut rng = rand::thread_rng();
     loop {
         number = rng.gen::<u64>();
         let to_hash = number.wrapping_add(last_id);
-        to_hash.hash(&mut hasher);
+        // to_hash.hash(&mut hasher);       //pose probleme
         // let answer: u64 = hasher.finish();
         let answer:u64 = hash_int(to_hash);
         if answer < HASH_MAX {
--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,13 @@ use block_chain::*;
 
 fn main() {
     //find value above block
-    let mut last_block = Block::new(0, 0, vec![]);
+    let mut last_block = Block::new(0, 0, vec![],0,1);
 
     loop{
         println!("Search for the current block : {:?}",last_block);
         let answer = mine(&last_block);
         println!("Answer : {}",answer);
-        last_block = last_block.new_block(vec![], answer);
+        last_block = last_block.new_block(vec![], answer,1);
         println!("The block is {} ",last_block.verification());
     }
 }
