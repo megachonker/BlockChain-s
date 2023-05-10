b13e5e8d1b02ea63a1efcad77dc17b008f08c713
cottelle <Cottelle@users.noreply.github.com>
2023-05-10 12:30:34 +0200

    add module. Implement first block_chain with an unique unser

---
+++ src/block_chain/mod.rs
@@ -0,0 +1,64 @@
+use std::collections::hash_map::DefaultHasher;
+use std::hash::Hash;
+use std::hash::Hasher;
+
+const HASH_MAX: u64 = 100000000000;
+
+pub struct Block {
+    id: u64,
+    last_block: u64,
+    transactions: Vec<Transaction>,
+    answer: u64,
+}
+
+pub struct Transaction {
+    initiating: u64,
+    receiver: u64,
+    amount: u32,
+}
+
+fn hash_int(value: u64) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    value.hash(&mut hasher);
+    hasher.finish()
+}
+
+impl Block {
+    pub fn new(last_block: u64, answer: u64, transactions: Vec<Transaction>) -> Block {
+        let id = answer; //for the momment
+        return Block {
+            id: id,
+            last_block: last_block,
+            transactions: transactions,
+            answer: answer,
+        };
+    }
+
+    pub fn verification(&self) -> bool {
+        hash_int(self.last_block + self.answer) < HASH_MAX
+    }
+    pub fn new_block(&self, new_transa: Vec<Transaction>, answer: u64) -> Block {
+        Block {
+            id: answer,
+            last_block: self.id,
+            transactions: new_transa,
+            answer: answer,
+        }
+    }
+}
+
+pub fn mine(last_block: &Block) -> u64 {
+    let mut hasher: DefaultHasher = DefaultHasher::new();
+    let last_answer = last_block.answer;
+    let mut number = 0;
+    loop {
+        number += 1;
+        let to_hash = number + last_answer;
+        to_hash.hash(&mut hasher);
+        let answer = hasher.finish();
+        if answer < HASH_MAX {
+            return answer;
+        }
+    }
+    return 0;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,40 @@
-use BlockChain::Block;
+mod block_chain;
 
+use block_chain::*;
 
+// const HASH_MAX: u64 = 3453209151749857438 / 100000;
 
-const HASH_MAX:u64 = 3453209151749857438/100000;
-
-use std::collections::hash_map::DefaultHasher;
-use std::hash::Hash;
-use std::hash::Hasher;
-pub fn new_block(last_block:Block,new_transa:Vec<Transaction>)->Block{
-    let last_answer = last_block.answer;
-    println!("INF TO\t\t{}",HASH_MAX);
-    let mut hasher: DefaultHasher = DefaultHasher::new();
-    let mut number =0;
-    while true {
-        number+=1;
-        let to_hash = number+last_answer;
-        to_hash.hash(&mut hasher);
-        let answer = hasher.finish();
-        println!("Tested: {}=>\t{}",number,answer);
-        if answer < HASH_MAX{
-            return Block {
-                id : answer,
-                last_block : last_block.id,
-                transactions : new_transa,
-                answer : number,
-            };
-        }
-    }
-    return  0;        
-}
-pub struct Transaction{
-    initiating : u64,
-    receiver : u64,
-    amount : u32,
-}
-
-
-pub struct Block {
-    id : u64,
-    last_block : u64,
-    transactions : Vec<Transaction>,
-    answer : u64,
-}
-
+// pub fn new_block(last_block: Block, new_transa: Vec<Transaction>) -> Block {
+//     let last_answer = last_block.answer;
+//     println!("INF TO\t\t{}", HASH_MAX);
+//     let mut hasher: DefaultHasher = DefaultHasher::new();
+//     let mut number = 0;
+//     while true {
+//         number += 1;
+//         let to_hash = number + last_answer;
+//         to_hash.hash(&mut hasher);
+//         let answer = hasher.finish();
+//         println!("Tested: {}=>\t{}", number, answer);
+//         if answer < HASH_MAX {
+//             return Block {
+//                 id: answer,
+//                 last_block: last_block.id,
+//                 transactions: new_transa,
+//                 answer: number,
+//             };
+//         }
+//     }
+//     return 0;
+// }
+// use BlockChain::Block;
 
 fn main() {
     //find value above block
-    print!("answer {}",new_block(50,)); 
+    let mut last_block = Block::new(0, 0, vec![]);
+
+    loop{
+        let answer = mine(&last_block);
+        println!("Answer : {}",answer);
+        last_block = last_block.new_block(vec![], answer);
+    }
 }
