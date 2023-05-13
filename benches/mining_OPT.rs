bd4092b9d97df4ae0fd65770af699de6b57906aa
megachonker <megachonker@users.noreply.github.com>
2023-05-13 02:26:03 +0200

    benchmark: add mining benhcmark

---
+++ benches/mining_OPT.rs
@@ -0,0 +1,15 @@
+use block_chain::{mine, mine_hasher_clone,Block}; // Import the functions from the block_chain module
+
+use criterion::{criterion_group, criterion_main, Criterion};
+
+// Define the benchmarking function
+fn benchmark_functions(c: &mut Criterion) {
+    let fist_block = Block::new(vec![]);
+    c.bench_function("Function Mine restart hasher", |b| b.iter(|| mine(&fist_block)));
+    c.bench_function("Function Mine clone hasher", |b| b.iter(|| mine_hasher_clone(&fist_block)));
+}
+
+// Specify the criterion group and main function
+criterion_group!(benches, benchmark_functions);
+criterion_main!(benches);
+
