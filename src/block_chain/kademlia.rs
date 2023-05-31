0629c999c73c2133d401f2c86655cdd06ef4bbcf
megachonker <megachonker@users.noreply.github.com>
2023-05-31 15:39:07 +0200

    cleaningl: remove ark

--- src/block_chain/kademlia.rs
+++ src/block_chain/kademlia.rs
@@ -1,7 +1,7 @@
 use std::mem;
 use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4, UdpSocket};
-use std::sync::{Arc, Mutex};
 use std::thread;
+use std::time;
 
 use bincode::{deserialize, serialize};
 use serde::{Deserialize, Serialize};
@@ -16,56 +16,41 @@ enum Packet {
 
 struct Node {
     node_addr: SocketAddr,
-    peers_addr: Arc<Vec<SocketAddr>>,
-    output_lock: Arc<Mutex<()>>,
+    peers_addr: Vec<SocketAddr>,
 }
 
 impl Node {
-    fn create(address: SocketAddr, bootstraps: Arc<Vec<SocketAddr>>, output_lock: Arc<Mutex<()>>) {
+    fn create(address: SocketAddr, bootstraps: Vec<SocketAddr>) {
         let mut node = Node {
             node_addr: address,
             peers_addr: bootstraps,
-            output_lock,
         };
-        // node.print();
 
         let socket_sender = UdpSocket::bind(node.node_addr).unwrap();
 
         thread::spawn(move || {
             let mut buffer = vec![0u8; mem::size_of::<SocketAddr>() * 5]; //on veux
             loop {
-                let (data,remote) = socket_sender.recv_from(&mut buffer).expect("err recv_from");
+                let (data, remote) = socket_sender.recv_from(&mut buffer).expect("err recv_from");
                 match deserialize(&buffer[..data]).expect("errreur deserial") {
                     Packet::GetPeers => {
-                        let serialized_packet = serialize::<[SocketAddr]>(&node.peers_addr).expect("Serialization error");
-                        socket_sender.send_to(&serialized_packet, remote).expect("err sendto");
+                        let serialized_packet =
+                            serialize(&node.peers_addr).expect("Serialization error");
+                        socket_sender
+                            .send_to(&serialized_packet, remote)
+                            .expect("err sendto");
                     }
                     Packet::RepPeers(mut response_packet) => {
-                        let mut peers = Arc::try_unwrap(node.peers_addr).expect("Failed to unwrap Arc"); // Unwrap the Arc to get ownership of the Vec
-                        peers.append(&mut response_packet); // Modify the Vec
-                        node.peers_addr = Arc::new(peers); // Convert the Vec back to an Arc and assign it to the node
+                        node.peers_addr.append(&mut response_packet); // Modify the Vec
                     }
                 }
             }
         });
-
-        // node.print();
-    }
-
-    fn print(&self) {
-        let _lock = self.output_lock.lock().unwrap();
-        println!("{}", self.node_addr);
-        println!("---------------");
-        for peer in self.peers_addr.iter() {
-            println!("{}", peer);
-        }
-        println!(""); //saut ligne
     }
 }
 
 pub fn kademlia_simulate() {
     //INIT
-    let output_lock = Arc::new(Mutex::new(())); // Create a single output lock
     let mut rng = rand::thread_rng();
     for id in 1..=254 {
         let mut bootstrap_socket: Vec<SocketAddr> = Vec::with_capacity(5);
@@ -80,10 +65,22 @@ pub fn kademlia_simulate() {
         }
         Node::create(
             SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 1, id as u8), 6021)),
-            bootstrap_socket.into(),
-            Arc::clone(&output_lock),
+            bootstrap_socket,
         );
     }
+    //wait that all thread reach the recv_from
+    //BTW i d'ont have solution to wait that recv_from fuction that reach and exectute,
+    //with Arc and sync barreiere you can garantie to be beffort or after
+    //if you are befort no garanty execution are correct
+    //if you are after because it was blockant there you c'ant reach that region
+    // and if you make that socket no blocking you can miss message
+    thread::sleep(time::Duration::from_millis(100));
+
+    //Fake starting
+
+    // Packet::GetPeers
+
+
 }
 
 // #[cfg(test)]
