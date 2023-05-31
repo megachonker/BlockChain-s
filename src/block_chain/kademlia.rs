5e38ecec3effb7ea03c832d356368337916dc2bd
megachonker <megachonker@users.noreply.github.com>
2023-05-31 16:41:14 +0200

    rust: rust ces buguer

--- src/block_chain/kademlia.rs
+++ src/block_chain/kademlia.rs
@@ -1,7 +1,7 @@
 use std::mem;
-use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4, UdpSocket};
-use std::thread;
+use std::net::{IpAddr,Ipv4Addr, SocketAddr, SocketAddrV4, UdpSocket};
 use std::sync::{Arc, Barrier};
+use std::thread;
 use std::time;
 
 use bincode::{deserialize, serialize};
@@ -20,6 +20,17 @@ struct Node {
     peers_addr: Vec<SocketAddr>,
 }
 
+// fn print_ip(socket_addr: SocketAddr) {
+//     match socket_addr.ip() {
+//         IpAddr::V4(ipv4) => {
+//             println!("IPv4 address: {}", ipv4);
+//         }
+//         IpAddr::V6(ipv6) => {
+//             println!("IPv6 address: {}", ipv6);
+//         }
+//     }
+// }
+
 impl Node {
     fn create(address: SocketAddr, bootstraps: Vec<SocketAddr>, starting_barr: Arc<Barrier>) {
         let mut node = Node {
@@ -27,24 +38,36 @@ impl Node {
             peers_addr: bootstraps,
         };
 
-        let socket_sender = UdpSocket::bind(node.node_addr).unwrap();
-        let mut buffer = vec![0u8; mem::size_of::<SocketAddr>() * 100]; //on veux 100 addres
-
-        
         thread::spawn(move || {
+            let socket = UdpSocket::bind(node.node_addr).unwrap();
             starting_barr.wait();
+            
+            let mut buffer = vec![0u8; mem::size_of::<SocketAddr>() * 100]; //on veux 100 addres
+            let serialized_packet = serialize(&node.peers_addr).expect("Serialization error");
+            
+            
+            //send initial
+            socket.send_to(&serialized_packet, &node.peers_addr[0]).expect("first batch send_to");
+        
+            // for peer in &node.peers_addr {
+            //     socket.send_to(&serialized_packet, peer).expect("first batch send_to");
+            // }
+
             loop {
-                let (data, remote) = socket_sender.recv_from(&mut buffer).expect("err recv_from");
+                let (data, remote) = socket.recv_from(&mut buffer).expect("err recv_from");
                 match deserialize(&buffer[..data]).expect("errreur deserial") {
                     Packet::GetPeers => {
-                        let serialized_packet =
-                            serialize(&node.peers_addr).expect("Serialization error");
-                        socket_sender
+                        let serialized_packet = serialize(&node.peers_addr).expect("Serialization error");
+                        socket
                             .send_to(&serialized_packet, remote)
                             .expect("err sendto");
+                        // println!("GetPeers from {}:", remote);
                     }
                     Packet::RepPeers(mut response_packet) => {
                         node.peers_addr.append(&mut response_packet); // Modify the Vec
+                        // print_ip(remote)
+                        // println!("RepPeers from {}:", remote.ip());
+                        // println!("{:?}", response_packet);
                     }
                 }
             }
@@ -57,7 +80,7 @@ pub fn kademlia_simulate() {
     let nb_ip = 254;
     let nb_boostrap = 5;
 
-    let start_barrierre =  Arc::new(Barrier::new(nb_ip));
+    let start_barrierre = Arc::new(Barrier::new(nb_ip));
     let mut rng = rand::thread_rng();
 
     for id in 1..=nb_ip {
@@ -67,43 +90,47 @@ pub fn kademlia_simulate() {
         for _ in 0..nb_boostrap {
             let socket = SocketAddr::V4(SocketAddrV4::new(
                 Ipv4Addr::new(127, 0, 1, rng.gen::<u8>()),
-                6021,
+                9026,
             ));
             bootstrap_socket.push(socket);
         }
         Node::create(
-            SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 1, id as u8), 6021)),
+            SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 1, id as u8), 9026)),
             bootstrap_socket,
             start_barrierre.clone(),
-
         );
     }
     //Fake starting
-    let mut buffer = vec![0u8; mem::size_of::<SocketAddr>() * 100]; //on veux 100 addres
-    
-    let src = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1 as u8), 6021));
-    let dst = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 1, 1 as u8), 6021));
-    
-    let test_socket = UdpSocket::bind(src).unwrap();
-    
-    let serialized_packet =    serialize(&Packet::GetPeers).expect("Serialization error");
-    
-    test_socket.send_to(&serialized_packet, dst).expect("err sendto");
-    let (data,remote) = test_socket.recv_from(&mut buffer).expect("err receve to");
+    thread::sleep(time::Duration::from_secs(1));
 
-    match deserialize(&buffer[..data]).expect("errreur deserial") {
-        Packet::GetPeers => {
-            println!("GetPeers from: {}", remote);
-        }
-        Packet::RepPeers(response_packet) => {
-            println!("RepPeers from: {}", remote);
-            for rep in response_packet  {
-                println!("{}", rep);
-            }
+    // thread::sleep(time::Duration::from_millis(100));
 
-        }
-    }
+    // let mut buffer = vec![0u8; mem::size_of::<SocketAddr>() * 100]; //on veux 100 addres
 
+    // let src = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 1, 1 as u8), 9026));
+    // let dst = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 1, 2 as u8), 9026));
+
+    // let test_socket = UdpSocket::bind(src).unwrap();
+    // let serialized_packet = serialize(&Packet::GetPeers).expect("Serialization error");
+
+    // test_socket
+    //     .send_to(&serialized_packet, dst)
+    //     .expect("err sendto");
+
+    // let (data, remote) = test_socket.recv_from(&mut buffer).expect("err receve to");
+    // print!("{:?}", &buffer[..data]);
+
+    // match deserialize(&buffer[..data]).expect("errreur deserial") {
+    //     Packet::GetPeers => {
+    //         println!("GetPeers from: {}", remote);
+    //     }
+    //     Packet::RepPeers(response_packet) => {
+    //         println!("RepPeers from: {}", remote);
+    //         for rep in response_packet {
+    //             println!("{}", rep);
+    //         }
+    //     }
+    // }
 }
 
 // #[cfg(test)]
