dd78949bb23333335b1049fdab209955a98c9094
megachonker <megachonker@users.noreply.github.com>
2023-11-20 00:57:53 +0100

    BUG: account have multiple key

--- src/block_chain/acount.rs
+++ src/block_chain/acount.rs
@@ -1,16 +1,15 @@
 use std::fmt::Display;
 
-use super::transaction::{Amount, Transaction, Utxo, TxIn, UtxoLocation};
-use anyhow::{Context, Error, Result};
+use super::transaction::{Amount, Transaction, TxIn, Utxo};
+use anyhow::{anyhow, Context, Error, Result};
 use dryoc::{sign::*, types::StackByteArray};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, Default)]
 pub struct ToSave {
-    wallet: Vec<(Amount, TxIn)>,
-    privkey: SecretKey,
+    wallet: Vec<Utxo>,
+    privkey: Vec<SecretKey>,
 }
-use tracing::{warn};
 
 impl std::fmt::Display for Acount {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -19,7 +18,7 @@ impl std::fmt::Display for Acount {
         writeln!(f, "Miner fee: {}%", self.miner_fee)?;
         writeln!(f, "Wallet:")?;
         for utxo in &self.wallet {
-            writeln!(f, "Value: {}, TxIn:{}", utxo.0,utxo.1)?;
+            writeln!(f, "Value: {}, TxIn:{}", utxo.0, utxo.1)?;
         }
         write!(f, "sold: {}", self.get_sold())
     }
@@ -61,9 +60,9 @@ pub struct Acount {
     /// fee to give to miner
     pub miner_fee: Amount,
     /// buch of non used transaction
-    pub wallet: Vec<(Amount, TxIn)>,
+    pub wallet: Vec<Utxo>,
     /// stuff to sign
-    keypair: Keypair,//to change on vec cuz there can have multiple key
+    keypair: Vec<Keypair>, //to change on vec cuz there can have multiple key
 }
 
 impl TryFrom<&str> for Acount {
@@ -78,7 +77,7 @@ impl Acount {
         &self.keypair //double clone
     }
 
-    pub fn get_pubkey(&self)->PublicKey{
+    pub fn get_pubkey(&self) -> PublicKey {
         self.keypair.0.public_key.clone()
     }
 
@@ -97,18 +96,16 @@ impl Acount {
         }
     }
 
-    pub fn refresh_wallet(&mut self, wallet: Vec<(UtxoLocation, Utxo)>) ->Result<()> {
+    pub fn refresh_wallet(&mut self, wallet: Vec<Utxo>) -> Result<()> {
         let mut new_wallet = vec![];
-        for (position,utxo) in wallet{
-            if self.get_pubkey() != utxo.target{
-                warn!("missing key for: {}",utxo);
-                continue;
-            }
-            let signed = utxo.sign(position, self.get_key()).with_context(|| format!("imposible de signer utxo {}",utxo) )?;
-            new_wallet.push((utxo.amount, signed));
+        wallet.iter().for_each(|utxo| {
+            self.keypair.iter().find(|ctx| {
+                if ctx != utxo.get_pubkey() {
+                    anyhow!("missing key for ulock utxo => {}", utxo)?;
+                }
+            })
+        });
 
-
-        }
         self.wallet = new_wallet;
         Ok(())
     }
@@ -135,7 +132,7 @@ impl Acount {
     pub fn save(self) -> Result<()> {
         let tosave = ToSave {
             wallet: self.wallet,
-            privkey: self.keypair.0.secret_key.to_owned(),
+            privkey: self.keypair,
         };
         let contents =
             serde_json::to_string(&tosave).context("serialisation de la conf user imposible")?;
@@ -147,8 +144,6 @@ impl Acount {
         let data = bincode::serialize(&transa).unwrap();
         self.keypair.0.sign_with_defaults(data).unwrap()
     }
-
-
 
     /// # find a combinaison of Utxo for a amount given
     ///
@@ -162,27 +157,26 @@ impl Acount {
     /// 7 2 2 was selected
     ///
     /// 10 to the user and send back 1
-    pub fn select_utxo(&self, amount: Amount) -> Option<(Vec<TxIn>, Amount)> {
+    pub fn select_utxo(&self, amount: Amount) -> Option<(Vec<TxIn>, Vec<Keypair>, Amount)> {
         if amount == 0 {
             return None;
         }
 
         let mut value_total = 0;
         let mut vec_utxo = vec![];
+        let mut vec_key = vec![];
 
-        for (amount,utxo) in &self.wallet {
+        for (amount, keypair, utxo) in &self.wallet {
             value_total += amount;
             vec_utxo.push(utxo.clone());
+            vec_key.push(keypair);
             if value_total >= *amount {
-                return Some((vec_utxo, value_total - amount));
+                return Some((vec_utxo, vec_key, value_total - amount));
             }
         }
 
         None
     }
-
-
-
 }
 
 #[cfg(test)]
